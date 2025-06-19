9bf1a9ee3095888dd358d1e19c242255c9c58bc7
Huvison <huvisoncollins@gmail.com>
2025-06-19 19:20:45 +0000

    feat (feat/threads): add Resolved deadlock code to depict how deadlocks was resolved

--- src/main/java/org/example/novaTech/deadlock/DeadLockResolved.java
+++ src/main/java/org/example/novaTech/deadlock/DeadLockResolved.java
@@ -2,45 +2,42 @@ package org.example.novaTech.deadlock;
 
 import java.util.logging.Logger;
 
-public class DeadLock {
-    private static final Logger logger = Logger.getLogger(DeadLock.class.getName());
+public class DeadLockResolved {
+    private static final Logger logger = Logger.getLogger(DeadLockResolved.class.getName());
     private static final Object resourceOne = new Object();
     private static final Object resourceTwo = new Object();
 
-
     public static void main(String[] args) {
         Thread threadOne = new Thread(() -> {
-            synchronized (resourceOne){
-                logger.info("Thread One locks resource 1");
+            synchronized (resourceOne) {
+                logger.info("Thread One locked resource 1");
 
-                //simulate thread one holding on to resource 1, which is needed by Thread Two
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
 
-                //Thread two tries to access resource 2
-                synchronized (resourceTwo){
-                    logger.info("Thread One accesses resource 2");
+                synchronized (resourceTwo) {
+                    logger.info("Thread One locked resource 2");
+                    logger.info("Thread One is doing work...");
                 }
             }
         });
 
         Thread threadTwo = new Thread(() -> {
-            synchronized (resourceTwo){
-                logger.info("Thread Two locks resource 2");
+            synchronized (resourceOne) {
+                logger.info("Thread Two locked resource 1");
 
-                //Thread two holding on to resource 2, which is needed by Thread One
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
 
-                //Thread one tries to access resource 1
-                synchronized (resourceOne){
-                    logger.info("Thread Two accesses resource 1");
+                synchronized (resourceTwo) {
+                    logger.info("Thread Two locked resource 2");
+                    logger.info("Thread Two is doing work...");
                 }
             }
         });
@@ -48,10 +45,10 @@ public class DeadLock {
         threadOne.start();
         threadTwo.start();
 
-        try{
+        try {
             threadOne.join();
             threadTwo.join();
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
